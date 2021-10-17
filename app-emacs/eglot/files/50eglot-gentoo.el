dfc7c3cb3c123af93fe762c4b7dc3ddae6dd2059
John Turner <jturner.usa+gentoo@gmail.com>
2021-10-17 15:07:17 -0400

    app-emacs/eglot: prepared eglot for PR to include into gentoo

--- app-emacs/eglot/files/50eglot-gentoo.el
+++ app-emacs/eglot/files/50eglot-gentoo.el
@@ -1,4 +1,2 @@
-;;; eglot site-lisp configuration
 (add-to-list 'load-path "@SITELISP@")
-(autoload 'eglot "eglot" nil t)
-(autoload 'eglot-ensure "eglot" nil t)
+(load "@SITELISP@/eglot-autoload.el")
