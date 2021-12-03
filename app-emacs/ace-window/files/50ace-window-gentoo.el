8546e3365180cb1a13f676f9faf3502245c941af
John Turner <jturner.usa+gentoo@gmail.com>
2021-12-02 19:44:07 -0500

    Removed unused ebuilds for now

--- app-emacs/ace-window/files/50ace-window-gentoo.el
+++
@@ -1,2 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(load "@SITELISP@/ace-window-autoload.el" nil t)
--- app-emacs/cfrs/files/50cfrs-gentoo.el
+++
@@ -1,2 +0,0 @@
-(add-to-list 'load-path "@SITELISP@")
-(load "@SITELISP@/cfrs-autoload.el" nil t)
